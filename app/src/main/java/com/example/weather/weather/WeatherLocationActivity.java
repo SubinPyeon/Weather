a3cbaeaccef0214945ccfd5e340e6eb080084b1e
SubinPyeon <sbp111@naver.com>
2023-10-19 17:41:32 +0900

    location 권한

--- app/src/main/java/com/example/weather/weather/WeatherLocationActivity.java
+++ app/src/main/java/com/example/weather/weather/WeatherLocationActivity.java
@@ -1,9 +1,18 @@
 package com.example.weather.weather;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,15 +21,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.weather.R;
 import com.example.weather.weather.util.Conversion;
-import com.example.weather.weather.util.RequestPermission;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -34,6 +52,16 @@ import retrofit2.Response;
 
 public class WeatherLocationActivity extends AppCompatActivity {
 
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 1981;
+    private static final int REQUEST_CODE_LOCATION_SETTINGS = 2981;
+    private static final String[] PERMISSIONS = new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION};
+    private FusedLocationProviderClient mFusedLocationClient;
+    private SettingsClient mSettingsClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private Location mLastLocation;
+
     private RecyclerView weatherRecyclerView;
     private TextView locationText;
 
@@ -45,29 +73,21 @@ public class WeatherLocationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_location);
 
+        init();
+        checkLocation();
+        requestLocation();
+
         TextView tvDate = findViewById(R.id.tv_date);
         Button btnRefresh = findViewById(R.id.btn_refresh);
         locationText = findViewById(R.id.tv_location);
         weatherRecyclerView = findViewById(R.id.rv_weather);
-
-
         weatherRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         tvDate.setText(new SimpleDateFormat("MM월 dd일", Locale.getDefault()).format(Calendar.getInstance().getTime()) + "날씨");
 
-        // 위치 권한 요청
-        //requestLocationPermission();
-
         btnRefresh.setOnClickListener(v -> {
-            //getLocationAndFetchWeather();
             requestLocation();
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        new RequestPermission(this).requestLocation(); // 위치 권한 요청
     }
 
     private void setWeather(String nx, String ny) {
@@ -131,10 +151,7 @@ public class WeatherLocationActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void onFailure(@NonNull Call<WEATHER> call, @NonNull Throwable t) {
-                TextView tvError = findViewById(R.id.tvError);
-                tvError.setText("api fail : " + t.getMessage() + "\n 다시 시도해주세요.");
-                tvError.setVisibility(View.VISIBLE);
-                Log.d("api fail", Objects.requireNonNull(t.getMessage()));
+                Log.e("api fail", Objects.requireNonNull(t.getMessage()));
             }
         });
     }
@@ -151,7 +168,6 @@ public class WeatherLocationActivity extends AppCompatActivity {
                     List<Address> addressList = getAddress(location.getLatitude(), location.getLongitude());
                     if (!addressList.isEmpty()) {
                         Address address = addressList.get(0);
-
                         locationText.setText(location.getLatitude() + ", " + location.getLongitude() + "\n" + address.getAdminArea() + " " + address.getLocality() + " " + address.getThoroughfare());
                     } else {
                         locationText.setText(location.getLatitude() + ", " + location.getLongitude());
@@ -193,5 +209,132 @@ public class WeatherLocationActivity extends AppCompatActivity {
         }
 
         return result;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_LOCATION_SETTINGS) {
+            switch (resultCode) {
+                case Activity.RESULT_OK:
+                    Toast.makeText(WeatherLocationActivity.this, "Result OK", Toast.LENGTH_SHORT).show();
+                    break;
+                case Activity.RESULT_CANCELED:
+                    Toast.makeText(WeatherLocationActivity.this, "Result Cancel", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void init() {
+        if (mFusedLocationClient == null) {
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        }
+
+        mSettingsClient = LocationServices.getSettingsClient(this);
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(20 * 1000);
+        //mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+    }
+
+    private void checkLocation() {
+        if (isPermissionGranted()) {
+            startLocationUpdates();
+        } else {
+            requestPermissions();
+        }
+    }
+
+    private boolean isPermissionGranted() {
+        for (String permission : PERMISSIONS) {
+            if (permission.equals(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION) && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                continue;
+            }
+            final int result = ContextCompat.checkSelfPermission(this, permission);
+
+            if (PackageManager.PERMISSION_GRANTED != result) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void requestPermissions() {
+        requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS_REQUEST_CODE);
+    }
+
+    private void startLocationUpdates() {
+        mSettingsClient.checkLocationSettings(mLocationSettingsRequest).addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @SuppressLint("MissingPermission")
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+            }
+        }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    resolveLocationSettings(e);
+                } else {
+                }
+            }
+        });
+    }
+
+    public void resolveLocationSettings(Exception exception) {
+        ResolvableApiException resolvable = (ResolvableApiException) exception;
+        try {
+            resolvable.startResolutionForResult(this, REQUEST_CODE_LOCATION_SETTINGS);
+        } catch (IntentSender.SendIntentException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length <= 0) {
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted.
+                startLocationUpdates();
+            } else {
+                // Permission denied.
+                for (String permission:permissions) {
+                    if ("android.permission.ACCESS_FINE_LOCATION".equals(permission)) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setTitle("알림");
+                        builder.setMessage("위치 정보 권한이 필요합니다.\n\n[설정]->[권한]에서 '위치' 항목을 사용으로 설정해 주세요.");
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int id)
+                            {
+                                Intent intent = new Intent();
+                                intent.setAction(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                intent.setData(Uri.fromParts("package", getPackageName(), null));
+                                startActivity(intent);
+                            }
+                        });
+                        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int id)
+                            {
+                                Toast.makeText(WeatherLocationActivity.this, "Cancel Click", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+                }
+            }
+        }
     }
 }
